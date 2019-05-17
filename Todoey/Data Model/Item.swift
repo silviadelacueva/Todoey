581eb1d1e7b2bd8b90eb8c052cb791b9dd85c596
Silvia de la Cueva Orts <silviadelacueva@gmail.com>
2019-05-17 18:18:16 +0200

    Convert to MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Silvia de la Cueva Orts on 17/05/2019.
+//  Copyright © 2019 Creative lust. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
